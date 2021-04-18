890ce7d1c4b3783752374b30d3cb5cc53b37b1f2
Holden <holdensheftel@gmail.com>
2021-04-18 15:50:28 -0600

    Add bars icon

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,15 +1,18 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCookieBite} from '@fortawesome/free-solid-svg-icons';
+import {faCookieBite, faBars} from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 export const Navbar = () => {
   return (
     <nav className="nav-container">
-      <div className="nav-item icon"><FontAwesomeIcon icon={faCookieBite} /></div>
-      <div className="nav-item">Home</div>
-      <div className="nav-item">About</div>
-      <div className="nav-item">Menu</div>
-      <div className="nav-item">Contact</div>
+      <div className="nav-item logo"><FontAwesomeIcon icon={faCookieBite} /></div>
+      <div className="nav-item" role="button">Home</div>
+      <div className="nav-item" role="button">About</div>
+      <div className="nav-item" role="button">Menu</div>
+      <div className="nav-item" role="button">Contact</div>
+      <div className="nav-item burger" role="button">
+        <FontAwesomeIcon icon={faBars} />
+      </div>
     </nav>
   );
 };
